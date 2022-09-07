f23cb21277bbd139a8aafcd047b694a55b61dbfd
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-07 14:53:36 +0200

    feat: Add Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 export default function App(){
     return <div className='card'>
-        {/* <Header />
-        <MainText />
-        <Footer /> */}
+        <Header />
+        {/* <MainText />
+        <Footer /> */} 
     </div>
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header(){
+    return <div className='header'>
+        <h1>Hadija Ahmatović</h1>
+        <h2>Web Developer</h2>
+        <h4><a href="mailto: hadija.ahmatovic@gmail.com">Send me an email</a></h4>
+    </div>
+}
