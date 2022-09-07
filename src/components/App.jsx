37a91ea804d069eb338a2d74cf8c6fef9a75259c
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-07 15:21:23 +0200

    feat: Add MainText component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import MainText from './MainText';
 
 export default function App(){
     return <div className='card'>
         <Header />
-        {/* <MainText />
-        <Footer /> */} 
+        <MainText />
+        {/* <Footer />  */}
     </div>
 }
---
+++ src/components/MainText.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MainText(){
+    return (
+        <div className="buttons">
+            <button className="btn"><i class="fa-solid fa-envelope fa-xl"></i>Email</button>
+            <button className="btn"><i class="fa-brands fa-linkedin fa-xl"></i>LinkedIn</button>
+        </div>
+    )
+}
