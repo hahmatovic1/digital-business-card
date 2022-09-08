7f14483c1a8cc484980733841f81ec3de7c8dca2
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-09 00:50:09 +0200

    feat: Add discord icon

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Buttons(){
     return <div className="buttons">
-    <button className="btn btn-envelope"><i class="fa-solid fa-envelope fa-xl"></i>Email</button>
     <button className="btn btn-linkedin"><i class="fa-brands fa-linkedin fa-xl"></i>LinkedIn</button>
+    <button className="btn btn-git"><i class="fa-brands fa-github fa-xl"></i>GitHub</button>
 </div>
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer(){
     return <ul className="footer">
         <li className="icon"><i class="fa-brands fa-twitter fa-xl"></i></li>
         <li className="icon"><i class="fa-brands fa-facebook fa-xl"></i></li>
+        <li className="icon"><i class="fa-brands fa-discord fa-xl"></i></li>
         <li className="icon"><i class="fa-brands fa-instagram fa-xl"></i></li>
-        <li className="icon"><i class="fa-brands fa-github fa-xl"></i></li>
     </ul>
 
 }
