1236fb1c21cc43ea1d199998c33a0a2befcde152
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-08 23:43:39 +0200

    feat: Add Footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import MainText from './MainText';
+import Footer from './Footer';
 
 export default function App(){
     return <div className='card'>
         <Header />
         <MainText />
-        {/* <Footer />  */}
+        <Footer /> 
     </div>
 }
