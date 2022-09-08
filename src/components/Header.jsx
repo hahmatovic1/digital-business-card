e817bc72bb0c30cad659cfdf6401d9540fdffb89
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-09 00:00:40 +0200

    feat: Add avatar image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import avatar from '../images/avatar.jpg';
 
 export default function Header(){
     return <div className='header'>
+        <img src={avatar} alt="woman looking at camera"></img>
         <h1>Hadija Ahmatović</h1>
         <h2>Web Developer</h2>
         <h4><a href="mailto: hadija.ahmatovic@gmail.com">Send me an email</a></h4>
