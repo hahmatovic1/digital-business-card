d18a67754d72148aa5963f93e968b93fcf4e7ba0
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-08 23:29:34 +0200

    refactor: Make buttons separate components

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Buttons(){
+    return <div className="buttons">
+    <button className="btn btn-envelope"><i class="fa-solid fa-envelope fa-xl"></i>Email</button>
+    <button className="btn btn-linkedin"><i class="fa-brands fa-linkedin fa-xl"></i>LinkedIn</button>
+</div>
+}
--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import Buttons from './Buttons';
 
 export default function MainText(){
     return (
         <>
-        <div className="buttons">
-            <button className="btn btn-envelope"><i class="fa-solid fa-envelope fa-xl"></i>Email</button>
-            <button className="btn btn-linkedin"><i class="fa-brands fa-linkedin fa-xl"></i>LinkedIn</button>
-        </div>
+        <Buttons />
         <div className="main-text">
             <h2>About</h2>
             <p>I am a diligent EE and CS student seeking a Frontend or Backend Web development internship. I'm ardent about building neat applications and effectively working through compelling engineering problems.</p>
