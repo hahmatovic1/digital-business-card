140f4e22092ab01159e1f9fb41d7325cdf0d0895
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-08 23:21:23 +0200

    feat: Add description to MainText

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 export default function MainText(){
     return (
+        <>
         <div className="buttons">
             <button className="btn btn-envelope"><i class="fa-solid fa-envelope fa-xl"></i>Email</button>
             <button className="btn btn-linkedin"><i class="fa-brands fa-linkedin fa-xl"></i>LinkedIn</button>
         </div>
+        <div className="main-text">
+            <h2>About</h2>
+            <p>I am a diligent EE and CS student seeking a Frontend or Backend Web development internship. I'm ardent about building neat applications. </p>
+            <h2>Interests</h2>
+            <p>I enjoy making websites, solving puzzles and reading anything engineering related. On weekeds I like spending time in nature and hiking with my friends.</p> 
+        </div>
+    </>
     )
 }
