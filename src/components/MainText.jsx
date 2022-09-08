d7635b86b07901107fd773100ed2289cef13542e
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-08 23:27:12 +0200

    style: Add margin to MainText

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -9,7 +9,7 @@ export default function MainText(){
         </div>
         <div className="main-text">
             <h2>About</h2>
-            <p>I am a diligent EE and CS student seeking a Frontend or Backend Web development internship. I'm ardent about building neat applications. </p>
+            <p>I am a diligent EE and CS student seeking a Frontend or Backend Web development internship. I'm ardent about building neat applications and effectively working through compelling engineering problems.</p>
             <h2>Interests</h2>
             <p>I enjoy making websites, solving puzzles and reading anything engineering related. On weekeds I like spending time in nature and hiking with my friends.</p> 
         </div>
