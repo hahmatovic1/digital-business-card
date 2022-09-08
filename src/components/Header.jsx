65eb9ca13cbe6637c87fe5ba945903647babe0c2
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-09-09 00:17:03 +0200

    style: Change header text margin and size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import avatar from '../images/avatar.jpg';
 export default function Header(){
     return <div className='header'>
         <img className='avatar' src={avatar} alt="woman looking at camera"></img>
-        <div>
+        <div className='header-text'>
             <h1>Hadija Ahmatović</h1>
             <h2>Web Developer</h2>
             <h4><a href="mailto: hadija.ahmatovic@gmail.com">Send me an email</a></h4>
